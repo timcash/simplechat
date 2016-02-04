652903dc78d6a4b63ac262931053e49c5dcc8e66
Tim Cash <timcash@gmail.com>
2016-02-03 20:54:00 -0800

    basic websocket setup closes #2

---
+++ server.build.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _http = require('http');
+
+var _http2 = _interopRequireDefault(_http);
+
+var _socket = require('socket.io');
+
+var _socket2 = _interopRequireDefault(_socket);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+app.use(_express2.default.static('.'));
+var server = _http2.default.Server(app);
+var socket = (0, _socket2.default)(server);
+
+socket.on('connection', function (conn) {
+    console.log('a user connected');
+    conn.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3000, function () {
+    console.log('listening on *:3000');
+});
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-import express  from 'express';
-import http     from 'http';
+import express from 'express';
+import http from 'http';
+import io from 'socket.io';
 
-let app     = express();
-let server  = http.Server(app);
+let app = express();
+app.use(express.static('.'));
+let server = http.Server(app);
+let socket = io(server);
+
+socket.on('connection', function(conn) {
+    console.log('a user connected');
+    conn.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+});
 
 app.get('/', function(req, res) {
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(3000, function() {
