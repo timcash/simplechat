a24c3423f730c30b0e102a5a84c6dfec253cf046
Tim Cash <timcash@gmail.com>
2016-02-04 17:02:25 -0800

    first auth tests

--- js/serverutils.js
+++ js/serverutils.js
@@ -4,10 +4,41 @@
 //
 // ===================================================
 
-export function adduser() {
-
+function authResponse(success, store) {
+    return {success, store};
 }
 
-export function auth(username, hash) {
-    return true ? hash === "guest" : false;
+export function loginUser(username, store) {
+    if(store[username]) {
+        // this user is already logged in
+        return authResponse(false, store);
+    }
+
+    store[username] = "loggedin";
+    return authResponse(true, store);
+}
+
+export function removeUser(username, store) {
+    delete store[username];
+    return store;
+}
+
+export function auth(username, hash, store) {
+    // check the password hash
+    if(hash !== "guest") {
+        return authResponse(false, store);
+    }
+
+    // is this user already logged in
+    let resp = loginUser(username, store);
+    if(resp.success === true) {
+        return resp;
+    }
+
+    // default
+    return authResponse(false, store);
 };
+
+export function addMessage(message, store) {
+
+}
---
+++ js/stypes.js
@@ -0,0 +1,2 @@
+export const ADD_MSG    = 'add_message';
+export const REPEAT_MSG = 'repeat_message';
